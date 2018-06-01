51530525756c488736613e1cd3adc55ad0a6b8a1
Tim Evans <tim.c.evans@me.com>
2018-06-01 10:27:37 -0400

    autoresize youtube videos

--- app/components/post-cards/youtube.js
+++ app/components/post-cards/youtube.js
@@ -1,21 +1,47 @@
 import Component from '@ember/component';
 import { computed, get } from '@ember/object';
+import { bind } from '@ember/runloop';
 
 export default Component.extend({
-  videoId: computed('payload.url', {
-    get() {
-      let url = get(this, 'payload.url');
-      return (
-        url.match(/youtube\.com\/watch\?v=([^/]+)/) ||
-        url.match(/youtu.be\/([^/]+)/) ||
-        url.match(/youtube\.com\/embed\/([^/]+)/)
-      )[1];
-    }
+
+  classNames: ['youtube-card'],
+
+  videoId: computed('payload.url', function () {
+    let url = get(this, 'payload.url');
+    return (
+      url.match(/youtube\.com\/watch\?v=([^/]+)/) ||
+      url.match(/youtu.be\/([^/]+)/) ||
+      url.match(/youtube\.com\/embed\/([^/]+)/)
+    )[1];
   }),
 
-  origin: computed({
-    get() {
-      return window.protocol + '//' + window.hostname;
-    }
+  didInsertElement() {
+    this._resize = bind(this, 'didResize');
+    window.addEventListener('resize', this._resize);
+  },
+
+  willDestroyElement() {
+    window.removeEventListener('resize', this._resize);
+  },
+
+  didResize() {
+    this.notifyPropertyChange('width');
+    this.notifyPropertyChange('height');
+  },
+
+  aspectRatio: computed('payload.width', 'payload.height', function () {
+    return this.payload.height / this.payload.width;
+  }),
+
+  width: computed('aspectRatio', function () {
+    return this.element.getBoundingClientRect().width - 32;
+  }),
+
+  height: computed('aspectRatio', 'width', function () {
+    return this.aspectRatio * this.width;
+  }),
+
+  origin: computed(function () {
+    return window.location.protocol + '//' + window.location.hostname;
   })
 });
