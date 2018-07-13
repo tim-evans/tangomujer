dcf9ba479e7f3362b8a1b6df530faa20c14a2bd4
Tim Evans <tim.c.evans@me.com>
2018-07-13 12:27:24 -0400

    fix names and guard around stripe

--- app/services/stripe.js
+++ app/services/stripe.js
@@ -3,18 +3,26 @@ import Service from '@ember/service';
 export default Service.extend({
 
   configure(group) {
-    this._stripe = Stripe(group.stripePublishableKey);
+    if (group.stripePublishableKey) {
+      this._stripe = Stripe(group.stripePublishableKey);
+    }
   },
 
   elements() {
-    return this._stripe.elements();
+    if (this._stripe) {
+      return this._stripe.elements();
+    }
   },
 
   paymentRequest(...args) {
-    return this._stripe.paymentRequest(...args);
+    if (this._stripe) {
+      return this._stripe.paymentRequest(...args);
+    }
   },
 
   createToken(element) {
-    return this._stripe.createToken(element);
+    if (this._stripe) {
+      return this._stripe.createToken(element);
+    }
   }
 });
